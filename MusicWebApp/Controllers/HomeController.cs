f08aae2c1d0e262a4269db26914aa8f8d8a917e7
de180334 <khoa2k4hg@gmail.com>
2025-03-05 13:33:38 +0700

    update ui profile user , update home

--- MusicWebApp/Controllers/HomeController.cs
+++ MusicWebApp/Controllers/HomeController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace MusicWebApp.Controllers
 {
     public class HomeController : Controller
     {
+        public IActionResult NewFeed()
+        {
+            return View();
+        }
+
+        public IActionResult ProfileUser()
+        {
+            return View();
+        }
+
+
         public IActionResult Index()
         {
             return View();
         }
     }
 }
+
+
